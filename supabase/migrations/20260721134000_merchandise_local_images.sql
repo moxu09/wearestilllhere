4fa9a70f64c2e803eb7f8f8c8bf8b5c32574d9fa
moxu <moxu0927@gmail.com>
2026-07-21 21:39:24 +0800

    fix: host merchandise photos locally

---
+++ supabase/migrations/20260721134000_merchandise_local_images.sql
@@ -0,0 +1,9 @@
+update public.site_content_items
+set image_url = '/merchandise/canvas-bag.jpg'
+where id = 'a1dbb5b1-4dff-4b4e-a134-97db97bffe45'
+  and content_type = 'merchandise';
+
+update public.site_content_items
+set image_url = '/merchandise/keychain.jpg'
+where id = '239dba14-5acf-43bf-a3a6-400d70e3552a'
+  and content_type = 'merchandise';
